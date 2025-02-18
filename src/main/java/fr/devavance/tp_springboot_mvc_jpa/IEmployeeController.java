26366472fa408852e62e4db1736cda068b2b53dd
bl <bouchkhaf@gmail.com>
2025-02-18 12:00:19 +0100

    TP2 exo3

---
+++ src/main/java/fr/devavance/tp_springboot_mvc_jpa/IEmployeeController.java
@@ -0,0 +1,10 @@
+package fr.devavance.tp_springboot_mvc_jpa;
+
+import fr.devavance.tp_springboot_mvc_jpa.beans.Employee;
+import org.springframework.ui.Model;
+
+public interface IEmployeeController {
+
+    public String displayHome(Model model);
+    public String addEmployee(Employee employee);
+}
--- src/main/java/fr/devavance/tp_springboot_mvc_jpa/controllers/EmployeeControleur.java
+++ src/main/java/fr/devavance/tp_springboot_mvc_jpa/controllers/EmployeeControleur.java
@@ -1,14 +1,16 @@
 package fr.devavance.tp_springboot_mvc_jpa.controllers;
 
+import fr.devavance.tp_springboot_mvc_jpa.IEmployeeController;
 import fr.devavance.tp_springboot_mvc_jpa.IEmployeeRepository;
 import fr.devavance.tp_springboot_mvc_jpa.beans.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class EmployeeControleur {
+public class EmployeeControleur implements IEmployeeController {
 
     @Autowired
     private IEmployeeRepository employeeRepository;
@@ -17,22 +19,16 @@ public class EmployeeControleur {
         this.employeeRepository = employeeRepository;
     }
 
-    @RequestMapping("/employees")
-    public String findAllEmployee(Model model){
+    @RequestMapping(value={"/employee/home","/employee"},method= RequestMethod.GET)
+    public String displayHome(Model model){
         model.addAttribute("employees",this.employeeRepository.findAll());
         model.addAttribute("employee",new Employee());
-        return "view_employees";
-    }
-
-    @RequestMapping("/")
-    public String home(Model model){
-        model.addAttribute("employees",this.employeeRepository.findAll());
         return "view_home";
     }
 
-    @RequestMapping("/addemployee")
+    @RequestMapping(value={"/employee/addemployee"},method= RequestMethod.POST)
     public String addEmployee(Employee employee){
         this.employeeRepository.save(employee);
-        return "redirect:/";
+        return "redirect:/employee";
     }
 }
